2b4b0c24bccef6f4c52df5601b12fd249084ad9d
gridranger <bardos.david@gmail.com>
2024-12-20 16:23:15 +0200

    add minor icon fixes

--- src/progman/ui/icon.py
+++ src/progman/ui/icon.py
@@ -9,7 +9,7 @@ class Icon(ABC, Frame, ProgmanWidget):
     HEIGHT = 76
 
     def __init__(self, *args: any, **kwargs: any) -> None:
-        Frame.__init__(self, *args, **kwargs, width=self.WIDTH, height=self.HEIGHT, cursor="hand2")
+        Frame.__init__(self, *args, **kwargs, width=self.WIDTH, height=self.HEIGHT)
         ProgmanWidget.__init__(self, 'icon')
         self.configure(background=self.theme.background)
         self._icon = None
@@ -32,20 +32,19 @@ class Icon(ABC, Frame, ProgmanWidget):
 
     def update_theme(self) -> None:
         ProgmanWidget.update_theme(self)
-        self.configure(background=self.theme.background)
+        self.configure(background="self.theme.background")
         self._icon_label.configure(background=self.theme.background)
         self._text_label.configure(background=self.theme.background)
 
     def render(self) -> None:
         ProgmanWidget.render(self)
         self.grid_propagate(False)
-        self._icon_label = Label(self, image=self.icon, justify="center", bg=self.theme.background)
-        self._text_label = Label(self, text=self._label, wraplength=84, justify="center",
-                                 bg=self.theme.background)
+        self._icon_label = Label(self, image=self.icon, justify="center", bg=self.theme.background, cursor="hand2")
+        self._text_label = Label(self, text=self._label, wraplength=74, justify="center", bg=self.theme.background, cursor="hand2")
         self._icon_label.bind("<Button-1>", self.on_click)
         self._text_label.bind("<Button-1>", self.on_click)
-        self._icon_label.grid(row=0, column=0, padx=26, pady=0)
-        self._text_label.grid(row=1, column=0, pady=(0, 10), padx=10)
+        self._icon_label.grid(row=0, column=0, ipadx=0, ipady=0, padx=26, pady=0)
+        self._text_label.grid(row=1, column=0, ipadx=0, ipady=0, padx=5, pady=0)
         for widget in [self, self._icon_label, self._text_label]:
             widget.bind("<Enter>", self.master.on_enter)
             widget.bind("<Leave>", self.master.on_leave)
